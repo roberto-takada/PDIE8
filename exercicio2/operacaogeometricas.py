75dfb04678fe94db15c8d496c3cddf16ec297c38
Roberto Takada <149258321+roberto-takada@users.noreply.github.com>
2024-09-25 11:09:04 -0300

    adicionadas funcoes utilizando outras bibliotecas

--- exercicio2/operacaogeometricas.py
+++ exercicio2/operacaogeometricas.py
@@ -2,23 +2,49 @@ import numpy as np
 from PIL import Image
 from scipy import ndimage
 
-from funcoes.plots import plotaImagemDeArray
+
+# from funcoes.plots import plotaImagemDeArray
+
+import matplotlib.pyplot as plt
 
 # Funcionamento do Main:
 #   Mudar a variável enderecoImagem para alterar a imagem a ser modificada
 #   Descomentar ou comentar as funções para rodar somente as funções desejadas
 def main():
-    enderecoImagem = './../house.tif'
+    # Usar '.\*.tif' no windows e './../*.tif' no Linux
+    enderecoImagem = '.\house.tif'
     imagem = Image.open(enderecoImagem)
     # rotacaoNGraus(imagem, 45)
     # zoomInEOut(imagem, 10)
-    translacaoXeY(imagem, 45, 70)
-
+    # ZoomInEOutSciPy(imagem, 0.5)
+    ZoomInEOutNumPy(imagem, 2)
+    # translacaoXeY(imagem, 45, 70)
+    
 def zoomInEOut(img, zoom):
     imagemArray = np.array(img)
     imagemZoom = ndimage.zoom(imagemArray, zoom)
     imagemArray = np.array(img)
     plotaImagemDeArray(imagemArray, imagemZoom)
+
+def ZoomInEOutSciPy(img, zoom):
+    imagemArray = np.array(img)
+    imagemZoom = ndimage.zoom(imagemArray, zoom)
+    plotaImagemDeArray(imagemArray, imagemZoom)
+
+def ZoomInEOutNumPy(img, zoom):
+    imagemArray = np.array(img)
+    new_img = np.array(img)
+    transformacao = [[zoom, 0, 0], [0, zoom, 0], [0, 0, 1]]
+    for i in range(len(imagemArray)):
+        for j in range(len(transformacao[0])):
+            for k in range(len(transformacao)):
+
+                # resulted matrix
+                new_img[i][j] += imagemArray[i][k] * transformacao[k][j]
+
+    print(new_img)
+    plotaImagemDeArray(imagemArray, new_img)
+
 
 def rotacaoNGraus(img, graus):
     imagemArray = np.array(img)
@@ -36,6 +62,16 @@ def translacaoXeY(img, x, y):
                                               transform)
     plotaImagemDeArray(imagemArray, imagemTranslacao)
 
+
+def plotaImagemDeArray(array, array2):
+  imgAntiga = Image.fromarray(array)
+  imgNova = Image.fromarray(array2)
+  fig, ax = plt.subplots(nrows=1, ncols=2)
+  ax[0].imshow(imgAntiga, cmap='gray')
+  ax[0].set_title("Imagem original")
+  ax[1].imshow(imgNova, cmap='gray')
+  ax[1].set_title("Imagem filtrada")
+  plt.show()
 
 if __name__ == "__main__":
     main()
--- exercicio2/operacaopormediana.py
+++ exercicio2/operacaopormediana.py
@@ -1,15 +1,31 @@
 import numpy as np
 from PIL import Image
+from PIL import ImageFilter
+from scipy.ndimage import generic_filter
+import	cv2 as cv
 
-from funcoes.plots import plotaImagemDeArray
+# from funcoes.plots import plotaImagemDeArray
 
 
+import matplotlib.pyplot as plt
+
+# Funcionamento do Main:
+#   Mudar a variável enderecoImagem para alterar a imagem a ser modificada
+#   Descomentar ou comentar as funções para rodar somente as funções desejadas
 def main():
-    enderecoImagem = './../lena_gray_512_salt_pepper.tif'
+    # Usar '.\*.tif' no windows e './../*.tif' no Linux
+    enderecoImagem = '.\lena_gray_512_salt_pepper.tif'
     imagem = Image.open(enderecoImagem)
-    # filtroMedia(imagem, 3)
-    filtroMediana(imagem, 3)
+    # filtroMedia(imagem, 2)
+    # filtroMediaPillow(imagem, 3)
+    # filtroMediaScipy(imagem, 3)
+    # filtroMediaOpenCV(enderecoImagem, 3)
+    # filtroMediana(imagem, 2)
+    # filtroMedianaPillow(imagem, 3)
+    # filtroMedianaScipy(imagem, 3)
+    # filtroMedianaOpenCV(enderecoImagem, 3)
 
+# FILTROS USANDO A MÉDIA    
 
 def filtroMedia(img, tam):
     imagemArray = np.array(img)
@@ -23,6 +39,23 @@ def filtroMedia(img, tam):
     imagemArray = np.array(img)
     plotaImagemDeArray(imagemArray, imagemFiltro)
 
+def filtroMediaPillow(img, tam):
+    imagemFiltrada = img.filter(ImageFilter.BoxBlur(tam))
+    img.show()
+    imagemFiltrada.show()
+
+def filtroMediaScipy(img, tam):
+    imagemArray = np.array(img)
+    imagemFiltrada = generic_filter(img, np.mean, [tam, tam])
+    plotaImagemDeArray(imagemArray, imagemFiltrada)
+
+def filtroMediaOpenCV(endereco, tam):
+    img = cv.imread(endereco)
+    imagemFiltrada = cv.blur(img, (tam, tam))
+    plotaImagemDeArray(img, imagemFiltrada)
+
+# FILTROS USANDO A MEDIANA
+
 def filtroMediana(img, tam):
     imagemArray = np.array(img)
     imagemFiltro = np.empty(imagemArray.shape)
@@ -34,6 +67,32 @@ def filtroMediana(img, tam):
 
     imagemArray = np.array(img)
     plotaImagemDeArray(imagemArray, imagemFiltro)
+
+def filtroMedianaPillow(img, tam):
+    imagemFiltrada = img.filter(ImageFilter.MedianFilter(tam))
+    img.show()
+    imagemFiltrada.show()
+
+def filtroMedianaScipy(img, tam):
+    imagemArray = np.array(img)
+    imagemFiltrada = generic_filter(img, np.median, [tam, tam])
+    plotaImagemDeArray(imagemArray, imagemFiltrada)
+
+def filtroMedianaOpenCV(endereco, tam):
+    img = cv.imread(endereco)
+    imagemFiltrada = cv.medianBlur(img, tam)
+    plotaImagemDeArray(img, imagemFiltrada)
+
+
+def plotaImagemDeArray(array, array2):
+  imgAntiga = Image.fromarray(array)
+  imgNova = Image.fromarray(array2)
+  fig, ax = plt.subplots(nrows=1, ncols=2)
+  ax[0].imshow(imgAntiga, cmap='gray')
+  ax[0].set_title("Imagem original")
+  ax[1].imshow(imgNova, cmap='gray')
+  ax[1].set_title("Imagem filtrada")
+  plt.show()
 
 if __name__ == "__main__":
     main()
